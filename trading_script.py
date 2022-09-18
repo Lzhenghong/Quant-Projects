b0284dec37a6e258398fd441ec7fa77f58d51e01
Lzhenghong <hong2288@gmail.com>
2022-09-19 03:11:02 +0800

    Contrarian, SMA, Bollinger initial

---
+++ trading_script.py
@@ -0,0 +1,13 @@
+from TechnicalTraders import *
+
+if __name__ == "__main__":
+        
+    trader = ConTrader(r"oanda.cfg", "EUR_USD", "1min", window = 1, units = 1000)
+    trader.get_most_recent()
+    trader.stream_data(trader.instrument, stop = 100)
+    if trader.position != 0: 
+        close_order = trader.create_order(trader.instrument, units = -trader.position * trader.units, 
+                                          suppress = True, ret = True) 
+        trader.report_trade(close_order, "GOING NEUTRAL")
+        trader.position = 0
+
