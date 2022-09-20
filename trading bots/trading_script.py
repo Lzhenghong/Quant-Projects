38fe5f596bbe2ac78bb0c881909435399fd323fc
Lzhenghong <hong2288@gmail.com>
2022-09-21 05:15:06 +0800

    stop=100 for testing

--- trading bots/trading_script.py
+++ trading bots/trading_script.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
         
     trader = ConTrader(r"oanda.cfg", "EUR_USD", "1min", window = 1, units = 1000, stop = "17:00")
     trader.get_most_recent()
-    trader.stream_data(trader.instrument)
+    # trader.stream_data(trader.instrument)
+    trader.stream_data(trader.instrument, stop=100)
     if trader.position != 0: 
         close_order = trader.create_order(trader.instrument, units = -trader.position * trader.units, 
                                           suppress = True, ret = True) 
